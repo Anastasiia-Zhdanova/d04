4a811e5f634c659c99377ed8cc69d0e81e9967bf
Anastasiia <nastuxa-007@mail.ru>
2021-06-21 01:45:36 +0300

    toDelite

--- d04_ex02/d04_ex02/Attributes/NoDisplayAttribute.cs
+++
@@ -1,9 +0,0 @@
-namespace d04_ex02.Attributes
-{
-    [System.AttributeUsage(System.AttributeTargets.Property, AllowMultiple = false)  
-    ] 
-    public class NoDisplayAttribute : System.Attribute
-    {
-        
-    }
-}
